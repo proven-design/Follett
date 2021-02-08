d191ac3fdf04a182f2e85eec244cc07096151647
Jonny <hello@proven.design>
2021-02-08 01:00:09 -0600

    init

---
+++ frontend/src/components/Nav.svelte
@@ -0,0 +1,65 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		border-bottom: 1px solid rgba(21, 10, 34, 0.1);
+		font-weight: 400;
+		padding: 0 1em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: black;
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+
+	b {
+		padding-right: 3em;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>the<b>blog.</b></a></li>
+		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+
+
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+		<li><a rel=prefetch aria-current='{segment === "articles" ? "page" : undefined}' href='articles'>articles</a></li>
+	</ul>
+</nav>
---
+++ frontend/src/routes/articles/index.svelte
@@ -0,0 +1,69 @@
+<script context="module">
+	import ApolloClient, { gql } from 'apollo-boost';  
+	import moment from 'moment'
+
+	const blogQuery = gql`
+	query Blogs {  
+		blogs {
+			id
+			Title
+			Description
+			Published
+			Body
+			author {
+				username
+			}
+			Slug
+			Cover {
+			url
+			}
+		}
+	}
+	`;
+
+	export async function preload({params, query}) {
+		const client = new ApolloClient({ 
+			uri: 'http://localhost:1337/graphql',
+			fetch: this.fetch
+			 });
+		const results = await client.query({
+			query: blogQuery
+		})
+
+		return {posts: results.data.blogs}
+	}
+
+</script>
+
+<script>
+	export let posts;
+</script>
+
+<style>
+	ul, p {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+
+	.main-title {
+		font-size: 25px;
+	}
+</style>
+
+<svelte:head>
+	<title>articles</title>
+</svelte:head>
+
+<h1>recent posts</h1>
+
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a class="main-title" rel='prefetch' href='articles/{post.Slug}'>{post.Title}</a></li>
+		<p> {moment().to(post.Published, "DD-MM-YYYY")} ago by {post.author.username} </p>
+	{/each}
+</ul>
+
